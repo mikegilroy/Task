dd1dbad205e6e192269ee8445871c16747880728
“pkadams67” <paul.k.adams@me.com>
2015-10-22 17:21:28 -0600

    Persistence with NSKeyedArchiver/NSKeyedUnarchiver

--- Task/TaskController.swift
+++ Task/TaskController.swift
@@ -10,6 +10,7 @@ import Foundation
 
 class TaskController {
     static let sharedInstance = TaskController()
+    static let taskKey = "taskKey"
     var tasksArray = [Task]()
     var completedTasksArray: [Task] {
         return tasksArray.filter( {(task: Task) -> Bool in
@@ -26,20 +27,41 @@ class TaskController {
         let taskOne = Task(name: "Get Groceries", notes: "Milk, Bread", date: NSDate().dateByAddingTimeInterval(259200), isComplete: false)
         let taskTwo = Task(name: "Get Dry Cleaning", notes: "Extra Starch", date: nil, isComplete: true)
         let taskThree = Task(name: "Go to the Pub", notes: "", date: NSDate().dateByAddingTimeInterval(0), isComplete: false)
+
         return [taskOne, taskTwo, taskThree]
     }
     
     init() {
         self.tasksArray = self.mockTasks
+        loadFromPersistentStorage()
     }
     
     func addTask (task:Task) {
         tasksArray.append(task)
+        saveToPersistentStorage()
     }
 
     func removeTask (task: Task) {
         let index = tasksArray.indexOf(task)
         self.tasksArray.removeAtIndex(index!)
+        saveToPersistentStorage()
     }
 
+    func saveToPersistentStorage () {
+        NSKeyedArchiver.archiveRootObject(self.tasksArray, toFile: self.filePath(TaskController.taskKey))
+    }
+    
+    func loadFromPersistentStorage () {
+        if let tasks = NSKeyedUnarchiver.unarchiveObjectWithFile(self.filePath(TaskController.taskKey)) {
+            self.tasksArray = tasks as! [Task]
+        }
+    }
+    
+    func filePath(key: String) -> String {
+        let directorySearchResults = NSSearchPathForDirectoriesInDomains(NSSearchPathDirectory.DocumentDirectory,NSSearchPathDomainMask.AllDomainsMask, true)
+        let documentsPath: AnyObject = directorySearchResults[0]
+        let entriesPath = documentsPath.stringByAppendingString("/\(key).plist")
+        
+        return entriesPath
+    }
 }
