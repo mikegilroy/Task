9d8f683e0e1fd9119a372d7d7e0ee1f54f43d3d4
“pkadams67” <paul.k.adams@me.com>
2015-10-22 16:03:33 -0600

    Implement Model

---
+++ Task/Task.swift
@@ -0,0 +1,50 @@
+//
+//  Task.swift
+//  Task
+//
+//  Created by Paul Kirk Adams on 10/22/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+
+class Task: NSObject, NSCoding, Equatable {
+    var name: String
+    var notes: String?
+    var date: NSDate?
+    var isComplete: Bool
+
+    struct PropertyKey {
+        static let nameKey = "name"
+        static let notesKey = "notes"
+        static let dateKey = "date"
+        static let isCompleteKey = "isComplete"
+    }
+    
+    init(name: String, notes: String?, date: NSDate?, isComplete: Bool) {
+        self.name = name
+        self.notes = notes
+        self.date = date
+        self.isComplete = isComplete
+    }
+    
+    init?(coder aDecoder: NSCoder) {
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        let notes = aDecoder.decodeObjectForKey(PropertyKey.notesKey) as? String
+        let date = aDecoder.decodeObjectForKey(PropertyKey.dateKey) as? NSDate
+        let isComplete = aDecoder.decodeObjectForKey(PropertyKey.isCompleteKey) as! Bool
+        self.init(name: name, notes: notes, date: date, isComplete: isComplete)
+    }
+
+    func encodeWithCoder(aCoder: NSCoder) {
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(notes, forKey: PropertyKey.notesKey)
+        aCoder.encodeObject(date, forKey: PropertyKey.dateKey)
+        aCoder.encodeObject(isComplete, forKey: PropertyKey.isCompleteKey)
+    }
+
+}
+    func == (lhs: Task, rhs: Task) -> Bool {
+    return lhs.name == rhs.name
+    
+}
---
+++ TaskListTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TaskListTableViewController.swift
+//  Task
+//
+//  Created by Paul Kirk Adams on 10/22/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Cocoa
+
+class TaskListTableViewController: UITableViewController {
+
+}
