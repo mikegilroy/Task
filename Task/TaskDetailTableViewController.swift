4a2e2622477288fcf9bb41fc17c10df532ac96a4
“pkadams67” <paul.k.adams@me.com>
2015-10-23 15:47:34 -0600

    Capture the Due Date

--- Task/TaskDetailTableViewController.swift
+++ Task/TaskDetailTableViewController.swift
@@ -8,16 +8,38 @@
 
 import UIKit
 
-class TaskDetailTableViewController: UITableViewController {
+class TaskDetailTableViewController: UITableViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var dueTextField: UITextField!
     @IBOutlet weak var notesTextView: UITextView!
+    @IBOutlet var dueDatePicker: UIDatePicker!
+    
+    var dueDateValue: String?
+    
+    @IBAction func datePickerValueChanged(sender: UIDatePicker) {
+    let date = sender.date
+        dueTextField.text = date.stringValue()
+        dueDateValue = date.stringValue()
+    }
+
+    @IBAction func userTappedView(sender: UITapGestureRecognizer) {
+        nameTextField.resignFirstResponder()
+        dueTextField.resignFirstResponder()
+        notesTextView.resignFirstResponder()
+    }
 
     func updateWithTasks (task: Task) {
         nameTextField.text = task.name
         dueTextField.text = task.date?.stringValue()
         notesTextView.text = task.notes
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+    }
+    
+    override func viewDidLoad() {
+        dueTextField.inputView = dueDatePicker
     }
     
 }
