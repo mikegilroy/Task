412b772447c439572748fd9e9f67a5873663f652
“pkadams67” <paul.k.adams@me.com>
2015-10-26 15:38:55 -0600

    Create and Modify the Task NSManagedObject

---
+++ Task.swift
@@ -0,0 +1,34 @@
+//
+//  Task.swift
+//  Task
+//
+//  Created by Paul Kirk Adams on 10/26/15.
+//  Copyright © 2015 DevMountain. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Task: NSManagedObject {
+
+    convenience init (name: String, notes: String? = nil, date: NSDate? = nil, isComplete: Bool, context: NSManagedObjectContext = Stack.sharedStack.managedObjectContext) {
+        let entity = NSEntityDescription.entityForName("Task", inManagedObjectContext: context)!
+        self.init(entity: entity, insertIntoManagedObjectContext: context)
+
+        self.name = name
+        self.notes = notes
+        self.date = date
+        self.isComplete = isComplete
+        
+    }
+
+
+
+//        @NSManaged var name: String
+//        @NSManaged var notes: String?
+//        @NSManaged var date: NSDate?
+//        @NSManaged var isComplete: NSNumber
+
+
+}
